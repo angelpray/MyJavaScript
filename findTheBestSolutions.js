ce8777fbb7b7d8a781efe9654ce6f7e9c8f5f1cb
angelpray <1136472002@qq.com>
2020-07-23 12:01:24 +0800

    function函数理解-todo

--- findTheBestSolutions.js
+++ findTheBestSolutions.js
@@ -5,16 +5,14 @@ var scores = [60, 50, 60, 58, 54, 54,
   46, 31, 57, 52, 44, 18,
   41, 53, 55, 61, 51, 44
 ];
-var i;
 var maxScore = Math.max(...scores);
 var index = [];
-for (i = 0; i <= scores.length - 1; i++) {
+for ( var i = 0; i <= scores.length - 1; ++i) {
   if (scores[i] === maxScore) {
     index[index.length] = "#" + i;
   }
   console.log(`泡泡配方 #${i+1} score: ${scores[i]}`)
 }
-console.log(index)
 console.log(`泡泡个数: ${scores.length}`)
 console.log(`最高分泡泡: ${maxScore}`)
 console.log(`得分最高的解决方案: ${index}`)
