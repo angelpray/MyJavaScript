8322a0296e8b074a2a80969c38f1a76b4cb88fdf
angelpray <1136472002@qq.com>
2020-08-06 23:03:31 +0800

    new.target -todo

--- this.js
+++ this.js
@@ -1,3 +1,4 @@
+// this in standard function
 window.color = 'blue'
 
 let o = {
@@ -7,7 +8,37 @@ let o = {
 function sayColor() {
   console.log(this.color)
 }
-sayColor()
+sayColor() // blue
 
 o.sayColor = sayColor;
-o.sayColor();
+o.sayColor(); // red
+
+// this in arrow function
+window.color = 'blue'
+let o = {
+  color: 'red'
+}
+let sayColor = () => {
+  console.log(this.color)
+}
+sayColor() // blue
+o.sayColor = sayColor
+o.sayColor() //blue
+
+// this in the event or timeout
+function King() {
+  this.royalName = 'sox'
+  setTimeout(() => {
+    // this will be the King distance
+    console.log(this.royalName)
+  })
+}
+function Queen() {
+  this.royalName = 'lxe'
+  setTimeout(function() {
+    // this will be the window object
+    console.log(this.royalName)
+  })
+}
+new King() // sox
+new Queen() //undefined
