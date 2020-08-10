71bc8526d7eb68910b8da7e971f57900d00f114d
angelpray <1136472002@qq.com>
2020-08-10 11:24:43 +0800

    CLOSURES FINISH

---
+++ Function/closures.js
@@ -0,0 +1,24 @@
+function createComparisonFunction(propertyName) {
+  return function (object1, object2) {
+    let value1 = object1[propertyName];
+    let value2 = object2[propertyName];
+    if (value1 > value2) {
+      return 1;
+    } else if (value1 < value2) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+// create function
+let compareNames = createComparisonFunction('name');
+// call function
+let result = compareNames({
+  name: 'Nicholas'
+}, {
+  name: 'Matt'
+});
+// dereference function - memory can now be reclaimed
+compareNames = null;
